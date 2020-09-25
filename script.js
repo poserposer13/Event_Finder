9321c128df95c2e89f6acb9b2233910ddadfaed9
Timothy Belmore <timothybelmore@trinitybiblecollege.edu>
2020-09-24 19:45:49 -0600

    bandInput being pulled from the user's input on the form. Got form to not redirect us to an error page.

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 $(document).ready(function () {
     let apiKey = "6776e4163d7073e13c7450202afa0977"
-    let queryUrl = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=chris+gaines&api_key=" + apiKey + "&format=json"
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function (response) {
 
 
-        console.log(response)
-    });
+    $("#music-form").on("submit", function (event) {
+
+
+        event.preventDefault()
+
+        let bandInput = $("#music-search").val()
+
+        console.log(bandInput)
+
+        let queryUrl = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + bandInput + "&api_key=" + apiKey + "&format=json"
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+
+
+            console.log(response)
+        });
+    })
 
 
 
